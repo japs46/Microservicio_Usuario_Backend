308e85c484e2cb8f69de9e7b268c9063f15981f5
Jhonathan <jhonathanps46@gmail.com>
2024-09-22 11:48:30 -0500

    Se agrego dependencia Dev Tools para que se reinicie la aplicacion con cada cambio, tambien se agrego documentacion swagguer para la api de crear un nuevo propietario

--- src/main/java/com/example/demo/infrastructure/controllers/PropietarioController.java
+++ src/main/java/com/example/demo/infrastructure/controllers/PropietarioController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.application.services.PropietarioService;
 import com.example.demo.domain.models.Propietario;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 @RestController
 @RequestMapping("/api/propietario")
 public class PropietarioController {
@@ -19,6 +22,9 @@ public class PropietarioController {
 		this.propietarioService = propietarioService;
 	}
 
+	@Operation(summary = "Crear un nuevo Propietario", description = "Guarda un nuevo propietario en la base de datos.")
+    @ApiResponse(responseCode = "200", description = "Propietario guardada exitosamente")
+    @ApiResponse(responseCode = "406", description = "No se aceptó la solicitud")
 	@PostMapping("/guardar")
 	public ResponseEntity<?> guardarPropietario(@RequestBody Propietario propietario){
 		
